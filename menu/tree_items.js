5f67c52b0d7eb10af7d64b21747121d3f84e0a28
Julen Etxaniz <juletxara@gmail.com>
2021-06-18 11:08:57 +0200

    21. bilerako akta gehitu

--- menu/tree_items.js
+++ menu/tree_items.js
@@ -159,6 +159,7 @@ var TREE_ITEMS =
           ['1.6.18 - 2021-05-28', '../Barne Informazioa/Barne Kudeaketa/Bilera Aktak/2021-05-28.pdf'],
           ['1.6.19 - 2021-06-04', '../Barne Informazioa/Barne Kudeaketa/Bilera Aktak/2021-06-04.pdf'],
           ['1.6.20 - 2021-06-11', '../Barne Informazioa/Barne Kudeaketa/Bilera Aktak/2021-06-11.pdf'],
+          ['1.6.21 - 2021-06-18', '../Barne Informazioa/Barne Kudeaketa/Bilera Aktak/2021-06-18.pdf'],
         ],
       ],
       ['2 - Artefaktuen Txantiloiak', null,
