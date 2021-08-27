207524660e4e155add076d4f319fe0933d4ede44
Julen Etxaniz <juletxara@gmail.com>
2021-08-27 11:58:36 +0200

    26. bilerako akta gehitu eta 25. eguneratu

--- menu/tree_items.js
+++ menu/tree_items.js
@@ -164,6 +164,7 @@ var TREE_ITEMS =
           ['1.6.23 - 2021-07-23', '../Barne Informazioa/Barne Kudeaketa/Bilera Aktak/2021-07-23.pdf'],
           ['1.6.24 - 2021-08-13', '../Barne Informazioa/Barne Kudeaketa/Bilera Aktak/2021-08-13.pdf'],
           ['1.6.25 - 2021-08-23', '../Barne Informazioa/Barne Kudeaketa/Bilera Aktak/2021-08-23.pdf'],
+          ['1.6.26 - 2021-08-27', '../Barne Informazioa/Barne Kudeaketa/Bilera Aktak/2021-08-27.pdf'],
         ],
       ],
       ['2 - Artefaktuen Txantiloiak', null,
